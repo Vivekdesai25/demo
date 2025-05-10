1c897e2337c73981ff1007b48db21e2aaddd4e14
Vivekdesai25 <153173803+Vivekdesai25@users.noreply.github.com>
2025-05-10 13:42:07 +0530

    Add files via upload
    Hello Python

---
+++ 1p.py
@@ -0,0 +1,10 @@
+print("Hello, World!")
+
+
+print("""My name is Vivek. 
+Now I'm just practicing Python programming, 
+so I'm using multiline comments.""")
+
+
+name = input("What's your name? ")
+print("Nice to meet you, " + name + "!")
